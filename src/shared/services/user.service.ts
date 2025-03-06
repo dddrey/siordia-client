290cde424b243f5bc86ef231b7baea6b9b821942
marktcheltsov <104628734+marktcheltsov@users.noreply.github.com>
2025-03-06 21:41:10 +0300

    env

--- src/shared/services/user.service.ts
+++ src/shared/services/user.service.ts
@@ -4,6 +4,7 @@ import api from "../utils/axios-instance";
 class UserService {
   async getUser(): Promise<User> {
     const response = await api.post<User>("/auth");
+    console.log(response.data);
     return response.data;
   }
 }
--- src/shared/utils/axios-instance.ts
+++ src/shared/utils/axios-instance.ts
@@ -1,14 +1,19 @@
 import axios from "axios";
 
+const getInitData = () => {
+  return (
+    window.Telegram.WebApp.initData ||
+    import.meta.env.VITE_DEV_TELEGRAM_INIT_DATA
+  );
+};
+
 const api = axios.create({
   baseURL: "/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
-    "init-data":
-      window.Telegram.WebApp.initData ||
-      import.meta.env.VITE_DEV_TELEGRAM_INIT_DATA,
+    "init-data": getInitData(),
   },
 });
 
