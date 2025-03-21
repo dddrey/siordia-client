de7bd2a7b4d5ede63bff04e1f3654962e787a2ef
marktcheltsov <104628734+marktcheltsov@users.noreply.github.com>
2025-03-21 20:45:55 +0300

    Update use-subscription.ts

--- src/shared/hooks/use-subscription.ts
+++ src/shared/hooks/use-subscription.ts
@@ -13,6 +13,7 @@ export const useSubscription = () => {
       window.Telegram.WebApp.openInvoice(res.data.paymentUrl, (status) => {
         if (status === "paid") {
           subscriptionsService.addSubscription(type);
+          setIsLoading(false);
           console.log(res);
         }
       });
