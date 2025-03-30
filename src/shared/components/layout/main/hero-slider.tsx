b62f683bb5bbff2ffb5edb697b4d407a6295bf12
marktcheltsov <104628734+marktcheltsov@users.noreply.github.com>
2025-03-30 19:45:15 +0300

    fix

--- src/shared/components/layout/main/hero-slider.tsx
+++ src/shared/components/layout/main/hero-slider.tsx
@@ -56,7 +56,7 @@ export const HeroSlider = () => {
               <img
                 src={slide.url}
                 alt={slide.altText}
-                className="object-cover w-full h-full rounded-[12px]"
+                className="object-cover w-full h-screen rounded-[12px]"
               />
               {slide.type === "link" && (
                 <Link
