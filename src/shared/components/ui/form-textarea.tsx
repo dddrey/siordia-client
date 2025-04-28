025502e6ab7b884cd37f867a8c209b2ad0137dc3
marktcheltsov <104628734+marktcheltsov@users.noreply.github.com>
2025-04-28 08:41:46 +0300

    Update form-textarea.tsx

--- src/shared/components/ui/form-textarea.tsx
+++ src/shared/components/ui/form-textarea.tsx
@@ -1,14 +1,21 @@
-import { UseFormRegister, FieldError, Path, FieldValues } from "react-hook-form"
+import {
+  UseFormRegister,
+  FieldError,
+  Path,
+  FieldValues,
+} from "react-hook-form";
+import { useState } from "react";
+import { cn } from "@/shared/utils/cn";
 
 interface FormTextAreaProps<T extends FieldValues> {
-  id: Path<T>
-  label: string
-  placeholder?: string
-  disabled?: boolean
-  register: UseFormRegister<T>
-  error?: FieldError
-  required?: boolean
-  rows?: number
+  id: Path<T>;
+  label: string;
+  placeholder?: string;
+  disabled?: boolean;
+  register: UseFormRegister<T>;
+  error?: FieldError;
+  required?: boolean;
+  rows?: number;
 }
 
 const FormTextArea = <T extends FieldValues>({
@@ -19,28 +26,48 @@ const FormTextArea = <T extends FieldValues>({
   register,
   error,
   required = false,
-  rows = 4
+  rows = 4,
 }: FormTextAreaProps<T>) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const textareaClass =
+    "border rounded-[8px] bg-inherit p-2 text-gray-500 placeholder:text-gray-500 text-[14px] placeholder:text-[14px] border-border outline-none resize-none";
+  const labelClass = "text-[13px] text-gray-500";
+  const errorClass = "text-red-500 text-sm";
+
   return (
-    <div className="flex flex-col gap-2 min-h-[120px]">
-      <div className="flex flex-col gap-2">
-        <label htmlFor={id}>{label}</label>
+    <div className="flex flex-col">
+      <div className="flex flex-col gap-1 transition-all duration-500">
+        <label
+          className={cn(labelClass, isFocused && "text-blue-500")}
+          htmlFor={id}
+        >
+          {label}
+        </label>
         <textarea
           id={id}
-          className="border rounded p-2 resize-none"
+          className={cn(
+            textareaClass,
+            isFocused &&
+              "border-blue-500 text-blue-500 placeholder:text-blue-500"
+          )}
           placeholder={placeholder}
           disabled={disabled}
           rows={rows}
-          {...register(id, { required })}
+          onFocus={() => setIsFocused(true)}
+          {...register(id, {
+            required,
+            onBlur: () => setIsFocused(false),
+          })}
         />
       </div>
-      <div className="min-h-[20px]">
+      <div className="min-h-[24px] text-[12px]">
         {error && (
-          <span className="text-red-500 text-sm">{error.message}</span>
+          <span className={cn(errorClass, "text-[12px]")}>{error.message}</span>
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FormTextArea 
+export default FormTextArea;
