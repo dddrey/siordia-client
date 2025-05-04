eba9a7c18ac53d751ecc1b8bbb25160b5bce9b2f
marktcheltsov <104628734+marktcheltsov@users.noreply.github.com>
2025-05-04 15:26:50 +0300

    Update axios-instance.ts

--- src/shared/utils/axios-instance.ts
+++ src/shared/utils/axios-instance.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const getInitData = () => {
-  // return window.Telegram.WebApp.initData;
-  return "AAGk-lZLAAAAAKT6VkuwW9OM%26user=%7B%22id%22%3A1263991460%2C%22first_name%22%3A%22Markuhaaaaa%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22markuhaaaa%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D%26auth_date=1709581318%26hash=f0d492ae4c33979dafafc3338ac8376f4e8679b1341c6a3863955da8766055ee";
+  return window.Telegram.WebApp.initData;
+  // return "AAGk-lZLAAAAAKT6VkuwW9OM%26user=%7B%22id%22%3A1263991460%2C%22first_name%22%3A%22Markuhaaaaa%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22markuhaaaa%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D%26auth_date=1709581318%26hash=f0d492ae4c33979dafafc3338ac8376f4e8679b1341c6a3863955da8766055ee";
 };
 
 const api = axios.create({
