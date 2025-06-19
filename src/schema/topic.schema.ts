9c1e94abf97d20d91021e6705b685c8be96ad516
marktcheltsov <104628734+marktcheltsov@users.noreply.github.com>
2025-06-19 13:22:04 +0300

    fix

--- src/schema/topic.schema.ts
+++ src/schema/topic.schema.ts
@@ -1,15 +1,16 @@
-import * as z from "zod"
+import * as z from "zod";
 
 export const topicSchema = z.object({
- name: z.string()
-   .min(2, "Название должно содержать минимум 2 символа")
-   .max(50, "Название не должно превышать 50 символов"),
- description: z.string()
-   .min(10, "Описание должно содержать минимум 10 символов")
-   .max(200, "Описание не должно превышать 200 символов"),
- about: z.string()
-   .min(2, "Короткое описание должно содержать минимум 2 символа")
-   .max(40, "Короткое описание не должно превышать 40 символов"),
-})
+  name: z
+    .string()
+    .min(2, "Название должно содержать минимум 2 символа")
+    .max(50, "Название не должно превышать 50 символов"),
+  description: z
+    .string()
+    .min(10, "Описание должно содержать минимум 10 символов"),
+  about: z
+    .string()
+    .min(2, "Короткое описание должно содержать минимум 2 символа"),
+});
 
-export type TopicFormValues = z.infer<typeof topicSchema>
+export type TopicFormValues = z.infer<typeof topicSchema>;
